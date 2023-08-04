82aa213fd80b05f44591225401fd2aab58bd945a
0xkyles <kylesyellow@gmail.com>
2023-08-05 00:26:16 +0100

    Set up the server to use socket IO

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,24 @@
 import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
