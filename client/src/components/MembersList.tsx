7adaaabaf9ef28b43d66dfe2a2bf18a500ab6d0a
0xkyles <kylesyellow@gmail.com>
2023-08-08 00:55:25 +0100

    Extracting the logic for updating members

--- client/src/components/MembersList.tsx
+++ client/src/components/MembersList.tsx
@@ -1,14 +1,41 @@
-import { User } from "@/stores/userStore";
+import { useEffect } from "react";
+import socket from "@/sockets";
+import { EVENTS } from "@/sockets";
+import { useToast } from "@/components/ui/use-toast";
+import { Notification } from "@/types";
+import useUserStore, { User } from "@/stores/userStore";
+import useMembersStore from "@/stores/membersStore";
 
-interface Props {
-  members: User[];
-  user: User | null;
-}
+const MembersList = () => {
+  const user = useUserStore((state) => state.user);
+  const { toast } = useToast();
+  const { members, setMembers } = useMembersStore();
 
-const MembersList = ({ members, user }: Props) => {
+  useEffect(() => {
+    socket.on(EVENTS.SERVER.UPDATE_MEMBERS, (members: User[]) => {
+      setMembers(members);
+    });
+
+    socket.on(
+      EVENTS.SERVER.NOTIFICATION,
+      ({ message, title }: Notification) => {
+        toast({
+          title,
+          description: message,
+        });
+      }
+    );
+
+    return () => {
+      socket.off(EVENTS.SERVER.NOTIFICATION);
+      socket.off(EVENTS.SERVER.UPDATE_MEMBERS);
+    };
+  }, []);
+
   return (
-    <div>
+    <>
       <p className="font-semibold mb-2">Members</p>
+
       <ul className="space-y-1 pl-3">
         {members.map((member) => (
           <li key={member.id} className="text-sm">
@@ -17,7 +44,7 @@ const MembersList = ({ members, user }: Props) => {
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/ChatRoomsPage.tsx
+++ client/src/pages/ChatRoomsPage.tsx
@@ -1,52 +1,28 @@
-import useUserStore, { User } from "@/stores/userStore";
-import useMembersStore from "@/stores/membersStore";
 import MembersList from "@/components/MembersList";
-import { useEffect } from "react";
-import socket from "@/sockets";
-import { EVENTS } from "@/sockets";
-import { useToast } from "@/components/ui/use-toast";
-import { Notification } from "@/types";
 import ChatZone from "@/components/ChatZone";
 import CreateRoomButton from "@/components/CreateRoomButton";
+import RoomsList from "@/components/RoomsList";
 
 const ChatRoomsPage = () => {
-  const user = useUserStore((state) => state.user);
-  const { toast } = useToast();
-  const { members, setMembers } = useMembersStore();
-
-  useEffect(() => {
-    socket.on(EVENTS.SERVER.UPDATE_MEMBERS, (members: User[]) => {
-      setMembers(members);
-    });
-
-    socket.on(
-      EVENTS.SERVER.NOTIFICATION,
-      ({ message, title }: Notification) => {
-        toast({
-          title,
-          description: message,
-        });
-      }
-    );
-
-    return () => {
-      socket.off(EVENTS.SERVER.NOTIFICATION);
-      socket.off(EVENTS.SERVER.UPDATE_MEMBERS);
-    };
-  }, []);
-
   return (
     <div className="h-screen flex flex-col gap-4">
-      <div className="px-8 h-[60px] flex justify-between items-center border-b border-slate-300">
+      <header className="px-8 h-[60px] flex justify-between items-center border-b border-slate-300">
         <h1 className="text-2xl font-semibold uppercase">Chatrooms</h1>
         <CreateRoomButton />
-      </div>
-      <div className="flex gap-4 h-full px-8 pb-2">
+      </header>
+
+      <main className="flex gap-4 h-full px-8 pb-2">
         <ChatZone />
+
         <div className="w-[20vw]">
-          <MembersList members={members} user={user} />
+          <div className="h-1/2">
+            <MembersList />
+          </div>
+          <div className="h-1/2">
+            <RoomsList />
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
